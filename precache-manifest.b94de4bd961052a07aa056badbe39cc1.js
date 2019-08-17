252c662d36db8b5b2ab9b453511a0cbd58ee4e60
Samuel Fils <smfils1@gmail.com>
2019-08-17 15:05:05 -0400

    Updates

---
+++ precache-manifest.b94de4bd961052a07aa056badbe39cc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5cbb4c04013026da0840553dac1af7",
+    "url": "https//smfils1.github.io/users-rolodex/index.html"
+  },
+  {
+    "revision": "050ca44da257462509db",
+    "url": "https//smfils1.github.io/users-rolodex/static/css/main.69466ed1.chunk.css"
+  },
+  {
+    "revision": "72558d03f2bb77701f61",
+    "url": "https//smfils1.github.io/users-rolodex/static/js/2.6a5af712.chunk.js"
+  },
+  {
+    "revision": "050ca44da257462509db",
+    "url": "https//smfils1.github.io/users-rolodex/static/js/main.c6b5938b.chunk.js"
+  },
+  {
+    "revision": "e018089743341477d54a",
+    "url": "https//smfils1.github.io/users-rolodex/static/js/runtime~main.efdf49d6.js"
+  }
+]);
